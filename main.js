52eb56eceaf59633e096341c309ea129f7e764c4
RalphAardenburg <131700049+RalphAardenburg@users.noreply.github.com>
2023-05-03 07:57:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// initialize the map and set its view to our chosen geographical coordinates and a zoom level:
+var map = L.map('map').setView([52.527684, 4.639624], 15);
+
+// er geldt een use policy voor deze tiles, niet zeker of dit gebruikt kan worden in de game?
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);  
