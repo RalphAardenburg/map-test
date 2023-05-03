8c74da590f3b1b3e800983f6fe014c4693f891f7
RalphAardenburg <131700049+RalphAardenburg@users.noreply.github.com>
2023-05-03 12:18:29 +0200

    aanpassing zoom level en max zoom level

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // initialize the map and set its view to our chosen geographical coordinates and a zoom level:
-var map = L.map('map').setView([52.527684, 4.639624], 15);
+var map = L.map('map').setView([52.527684, 4.639624], 14);
 
 // er geldt een use policy voor deze tiles, niet zeker of dit gebruikt kan worden in de game?
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 16,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);  
